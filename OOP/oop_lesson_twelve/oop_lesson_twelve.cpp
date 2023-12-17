4fb6afd6fc4ce366bdd8d9b521e1278c4c4b2801
Umud_6qgy <Umud_6qgy@STHQ0118-15>
2023-12-17 21:51:25 +0400

    .

--- OOP/oop_lesson_twelve/oop_lesson_twelve.cpp
+++ OOP/oop_lesson_twelve/oop_lesson_twelve.cpp
@@ -1,20 +1,27 @@
-// oop_lesson_twelve.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+void foo(int arg)
+{
+	std::cout << arg << " - int\n";
+}
+
+void foo(float arg)
+{
+	std::cout << arg << " - float\n";
+}
+
+void foo(char arg)
+{
+	std::cout << arg << " - char\n";
+}
+
+template<typename... Args>
+void print_x2(Args&&... args)
+{
+	((foo(args)), ...);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	print_x2(1, '2', 3.f);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
