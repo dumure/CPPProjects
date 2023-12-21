2c4845e691d89b91bf7cb7578074ab3cba71848e
user <user@DESKTOP-38PKG31>
2023-12-21 20:59:52 +0400

    *

--- OOP/oop_lesson_fourteen/oop_lesson_fourteen.cpp
+++ OOP/oop_lesson_fourteen/oop_lesson_fourteen.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
-
-template<typename T>
-struct is_class
-{
-    constexpr static bool value = true;
-};
-
-template<typename T>
-struct is_class<T*>
-{
-    constexpr static bool value = false;
-};
-template<typename T>
-struct is_class<T&>
-{
-    constexpr static bool value = false;
-};
-template<>
-struct is_class<int>
-{
-    constexpr static bool value = false;
-};
+#include <vector>
 
 int main()
 {
-    std::cout << std::boolalpha << is_class<int****>::value;
+    std::vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(1);
+    arr.push_back(1);
 }
