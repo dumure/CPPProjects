8f265b7f607fcd52561f326653073a16efa2152b
user <user@DESKTOP-38PKG31>
2023-12-27 23:20:05 +0400

    *

--- OOP/oop_dz_fourteen/oop_dz_fourteen.cpp
+++ OOP/oop_dz_fourteen/oop_dz_fourteen.cpp
@@ -1,13 +1,53 @@
 #include <iostream>
+#include <string>
 #include "Stack.h"
+
+bool is_brackets_correct(std::string a_string)
+{
+    Stack<char> stack;
+    for (int i = 0; i < a_string.size(); i++)
+    {
+        if (a_string[i] == '(' || a_string[i] == '[' || a_string[i] == '{')
+        {
+            stack.push(a_string[i]);
+        }
+        if (!a_string.empty())
+        {
+            if (stack.peek() == '(')
+            {
+                if (a_string[i] == ')')
+                {
+                    stack.pop();
+                }
+            }
+            else if (stack.peek() == '[')
+            {
+                if (a_string[i] == ']')
+                {
+                    stack.pop();
+                }
+            }
+            else if (stack.peek() == '{')
+            {
+                if (a_string[i] == '}')
+                {
+                    stack.pop();
+                }
+            }
+        }
+    }
+    return stack.empty();
+}
 
 int main()
 {
-    Stack<std::string> stack;
-    stack.push("Hello ");
-    stack.push("C++ and ");
-    stack.push("Python");
-    std::cout << stack;
-    std::cout << stack.size() << '\n';
-    std::cout << sizeof(std::string) << '\n';
+    bool check = is_brackets_correct("(1{2[test}])");
+    if (check)
+    {
+        std::cout << "OK" << '\n';
+    }
+    else
+    {
+        std::cout << "NOT OK" << '\n';
+    }
 }
